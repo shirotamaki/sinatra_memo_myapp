ae85937428eb0739cb72371d7455eb32aed64104
shirotamaki <shiro.tamaki63@gmail.com>
2021-07-21 15:38:30 +0900

    connメソッドを定義してまとめました。`PG.connect(dbname: 'memos')`

--- myapp.rb
+++ myapp.rb
@@ -12,8 +12,11 @@ helpers do
   end
 end
 
+def conn
+  PG.connect(dbname: 'memos')
+end
+
 get '/memos' do
-  conn = PG.connect(dbname: 'memos')
   @memos = conn.exec('SELECT * FROM t_memos;')
   erb :index
 end
@@ -23,31 +26,26 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  conn = PG.connect(dbname: 'memos')
   conn.exec('INSERT INTO t_memos (title, content) VALUES ($1, $2);', [params['title'], params['content']])
   redirect to('/memos')
 end
 
 get '/memos/:id' do
-  conn = PG.connect(dbname: 'memos')
   @memos = conn.exec('SELECT * FROM t_memos WHERE id = $1;', [params['id']])
   erb :show
 end
 
 get '/memos/:id/edit' do
-  conn = PG.connect(dbname: 'memos')
   @memos = conn.exec('SELECT * FROM t_memos WHERE id = $1;', [params['id']])
   erb :edit
 end
 
 delete '/memos/:id' do
-  conn = PG.connect(dbname: 'memos')
   conn.exec('DELETE FROM t_memos WHERE id = $1;', [params['id']])
   redirect to('/memos')
 end
 
 patch '/memos/:id' do
-  conn = PG.connect(dbname: 'memos')
   conn.exec('UPDATE t_memos SET title = $1, content = $2 WHERE id = $3;', [params['title'], params['content'], params['id']])
   redirect to('/memos')
 end
